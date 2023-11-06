5cd0b82fc3f33bf232f180bab0607d5d6b2b12df
GERARD2619-17 <gera121134@gmail.com>
2023-11-05 19:05:05 -0600

    pruebanuevodomain

--- logearme.js
+++ logearme.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('https://sweet-elephant-lively.ngrok-free.app/api/auth/local', {
+            const response = await fetch('https://693b25430ef334116eb873e1840de016.serveo.net/api/auth/local', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
